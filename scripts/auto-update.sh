be857ada6446f170ecf56175aa3c4e8f0468c7c8
Pascal Boutin <pboutin@mirego.com>
2019-02-24 15:49:31 -0500

    Complete the auto-update script

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
+echo "Switching git branches"
+git checkout master
+git pull
+git checkout auto-updates
+git reset --hard master
+
 echo "Downloading the Atlas background..."
 curl --silent 'https://web.poecdn.com/image/Art/2DArt/Atlas/Atlas.png' > atlas.png
+
+echo "Scraping the wiki for maps..."
+node scripts/scrape-maps-wiki.js
+
+echo "Recompiling maps..."
+node scripts/compile-maps.js
+
+if [[ -z $(git diff --quiet) ]]
+then
+  echo "No changes detected, quitting."
+else
+  echo "Pushing changes to git..."
+  git commit --all --message="Auto update";
+  git push origin auto-updates
+
+  echo "Alerting the Discord channel..."
+  curl --data '{"content":"Changes detected !\nDetails: https://github.com/poe-world/poe-world-resources/compare/master...auto-updates"}' --header "Content-Type: application/json" -X POST https://discordapp.com/api/webhooks/$1
+fi
+
+echo "Done."
